e9f44bc61b402514d063eb421e77663baf1941d5
matsufuji06 <n.matsufuji06@gmail.com>
2020-07-11 10:56:54 +0900

    auto-update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-        `<div class="message">
+        `<div class="message" data-message-id=${message.id}>
           <div class="upper-message">
             <div class="chat-main__message__name">
               ${message.user_name}
@@ -23,7 +23,7 @@ $(function(){
       return html;
     } else {
       var html =
-        `<div class="message">
+        `<div class="message" data-message-id=${message.id}>
           <div class="upper-message">
             <div class="chat-main__message__name">
               ${message.user_name}
@@ -40,10 +40,11 @@ $(function(){
         </div>`
       return html;
     };
-  }
+  };
 $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
+  console.log(this)
   var url = $(this).attr('action')
  
   $.ajax({
@@ -53,20 +54,45 @@ $('#new_message').on('submit', function(e){
     dataType: 'json',
     processData: false,
     contentType: false
+  })
+  .done(function(data){
+    console.log(data)
+    var html = buildHTML(data);
+    $('.chat-main__message').append(html);
+    $('.chat-main__message').animate({ scrollTop: $('.chat-main__message')[0].scrollHeight});
+  })
+  .fail(function() {
+    alert("メッセージ送信に失敗しました");
+  })
+  .always(function(){
+    $('form')[0].reset();
+    $(".send-btn").prop('disabled', false);
+  })
+});
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
     })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.chat-main__message').append(html);
-      $('.chat-main__message').animate({ scrollTop: $('.chat-main__message')[0].scrollHeight});
-     
+    .done(function(messages) {
+      console.log(messages)
+      if (messages.length !== 0 ) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat-main__message').append(insertHTML);
+        $('.chat-main__message').animate({ scrollTop: $('.chat-main__message')[0].scrollHeight});
+      }
     })
     .fail(function() {
-      alert("メッセージ送信に失敗しました");
-      
-    })
-    .always(function(){
-      $('form')[0].reset();
-      $(".send-btn").prop('disabled', false);
-    })
-})
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
